26d95cc4514fb185e96059c83f0ad3ee8526a776
gliu-aa <gliu@appannie.com>
2016-04-22 16:45:28 +0800

    last

--- src/icon_find_threshold.py
+++ src/icon_find_threshold.py
@@ -1,8 +1,22 @@
 
+
 def find_threshold(file_name, report_name):
-    pass
+    # example: icon_id, dhash_v, phash_v, tag
+    data = read_data(file_name)
 
 
+
+def sigmoid(inX):
+    return 1.0/(1+exp(-inX))
+
+
+def read_data(file_name):
+    with file(file_name, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        data = list()
+        for icon_id, dhash_v, phash_v, tag in reader:
+            data.append((icon_id, dhash_v, phash_v, tag))
+        return data
 
 if __name__ == '__main__':
-    find_threshold('res/icon/icon_analytical_report')
+    find_threshold('res/icon/demo.csv')
