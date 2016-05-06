b752f6251ad443592ebd79d3db829788514345c6
lingowei <weiliu32992016@qq.com>
2016-05-06 22:17:17 +0800

    export html

--- src/export_html.py
+++ src/export_html.py
@@ -13,7 +13,6 @@ html_header = """<!DOCTYPE html>
 
 html_body = """<tr>
     <td width="100">id: {id}</td>
-    <td width="100">dhash: {hash}</td>
     <td width="100">tag: {tag}</td>
     <td><img width=150px src="{old}"></td>
     <td><img width=150px src="{new}"></td>
@@ -26,14 +25,16 @@ html_footer = """</table>
 """
 
 
-def generate_report(raw_file_name, report_file_name):
+def generate_report(dir_path, raw_file_name, report_file_name):
     if os.path.exists(report_file_name):
         os.remove(report_file_name)
 
     with open(report_file_name, 'a') as writer:
         writer.write(html_header)
-        for icon_id, old, new, similarity, tag in read_data(raw_file_name):
-            writer.write(html_body.format(id=icon_id, hash=similarity, tag=tag, old=old, new=new))
+        for icon_id, similarity, tag in read_data(raw_file_name):
+            old = dir_path + '/{id}/left.png'.format(id=icon_id)
+            new = dir_path + '/{id}/ight.png'.format(id=icon_id)
+            writer.write(html_body.format(id=icon_id, tag=tag, old=old, new=new))
             print 'ok for %s' % icon_id
         writer.write(html_footer)
 
@@ -41,12 +42,13 @@ def generate_report(raw_file_name, report_file_name):
 def read_data(file_name):
     with file(file_name, 'r') as csv_file:
         reader = csv.reader(csv_file)
-        for icon_id, old_val, new_val, similarity, tag in reader:
-            yield icon_id, old_val, new_val, similarity, tag
+        for icon_id, _, _, similarity, tag in reader:
+            yield icon_id, similarity, tag
 
 
 if __name__ == '__main__':
-    raw_file = 'res/icon/report_icon_merge_sort_2.csv'
-    report_file = 'res/icon/report_icon_merge_sort_2.html'
-    generate_report(raw_file, report_file)
+    dir_path = 'd:/workspace/workspace/icons'
+    raw_file = 'res/icon/icons_sort.csv'
+    report_file = 'res/icon/icons_sort.html'
+    generate_report(dir_path, raw_file, report_file)
     print 'Succeed to finish.'
--- src/generate_icon_report.py
+++ src/generate_icon_report.py
@@ -2,51 +2,56 @@ import argparse
 import csv
 import os
 
-import imagehash
+# import imagehash
 
 from icon_spider import UrlLibIconSpider
 
 
 def generate_report(begin_id, raw_file_name, report_file_name):
-    if os.path.exists(report_file_name):
-        os.remove(report_file_name)
+    # if os.path.exists(report_file_name):
+    #     os.remove(report_file_name)
     icon_spider = UrlLibIconSpider()
     with open(report_file_name, 'a') as report_file:
         writer = csv.writer(report_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        
         for icon_id, old, new in read_raw_data(raw_file_name):
             if int(icon_id) < begin_id:
                 continue
-            similarity = compute_similarity(icon_spider, old, new)
-            save(writer, [icon_id, old, new, similarity])
-            print 'ok for %s, similarity: %s' % (icon_id, similarity)
+            print 'start', icon_id
+            url_icon_dict = icon_spider.scrape({old, new})
+
+            if old in url_icon_dict and new in url_icon_dict and url_icon_dict[old] and url_icon_dict[new]:
+                print 'save', icon_id
+                dir_path = "icons\{id}"
+                img_path = "icons\{id}\{lr}.jpg"
+                os.mkdir(dir_path.format(id=icon_id))
+                url_icon_dict[old].save(img_path.format(id=icon_id, lr='left'))
+                url_icon_dict[new].save(img_path.format(id=icon_id, lr='ight'))
+            # similarity = compute_similarity(url_icon_dict)
+            # save(writer, [icon_id, old, new, similarity])
+            print 'ok for %s' % (icon_id)
 
 
 def read_raw_data(file_name):
     with file(file_name, 'r') as csv_file:
         reader = csv.reader(csv_file)
-        for icon_id, old_val, new_val in reader:
+        for icon_id, old_val, new_val, _, _ in reader:
             yield icon_id, old_val, new_val
 
 
-def compute_similarity(icon_spider, base_val, comparison_val):
-    url_icon_set = icon_spider.scrape({base_val, comparison_val})
-    if not any(url_icon_set.values()) or base_val not in url_icon_set or comparison_val not in url_icon_set:
-        return 'N/A'
-    try:
-        similarity = abs(imagehash.dhash(url_icon_set[base_val]) - imagehash.dhash(url_icon_set[comparison_val]))
-        return similarity
-    except Exception as ex:
-        print ex
-        return 'N/A'
+# def compute_similarity(url_icon_dict):
+#     if not any(url_icon_set.values()) or base_val not in url_icon_set or comparison_val not in url_icon_set:
+#         return 'N/A'
+#     try:
+#         similarity = abs(imagehash.dhash(url_icon_set[base_val]) - imagehash.dhash(url_icon_set[comparison_val]))
+#         return similarity
+#     except Exception as ex:
+#         print ex
+#         return 'N/A'
 
 
 def save(writer, row):
-    """
-
-    :param writer:
-    :param row:
-    :return:
-    """
+ 
     writer.writerow(row)
 
 
@@ -61,7 +66,8 @@ if __name__ == '__main__':
         help='after id'
     )
     args = parser.parse_args()
-    raw_file = 'res/icon/ios_icon_%s.csv' % args.filename
-    report_file = 'res/icon/report_ios_icon_%s.csv' % args.filename
+    # raw_file = 'res/icon/ios_icon_%s.csv' % args.filename
+    raw_file = 'res/icon/icons.csv'
+    report_file = 'res/icon/report_ios_icon_%s.csv' % 1
     generate_report(args.after, raw_file, report_file)
     print 'Succeed to finish.'
